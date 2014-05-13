80fabae8b03ff89909111a38e3c2de18c2ee9a49
jpc2000 <josh.cullum@cfms.org.uk>
2014-05-13 12:53:46 +0100

    changes made to script to get fully working

--- quota.sh
+++ quota.sh
@@ -4,52 +4,30 @@
 
 FILESET=($(mmlsfileset mogpfs | awk '{print $1}'))
 
-#Show the array Size
-
-echo "Array size: ${#FILESET[*]}"
-
-#Show the items in the Array
-
-echo "Array items:"
-for item in ${FILESET[*]}
-do
-    printf "   %s\n" $item
-done
-
-#Create the Array Indexes
-
-echo "Array indexes:"
-for index in ${!FILESET[*]}
-do
-    printf "   %d\n" $index
-done
-
-#Show the Array Items and Indexes
-
-echo "Array items and indexes:"
-for index in ${!FILESET[*]}
-do
-    printf "%4d: %s\n" $index ${FILESET[$index]}
-done
-
 #For Item in Fileset define Usage, Hard Quota and Percentage used
 
-echo "quota for fileset"
-
+function myecho
+{
+  echo $1
+}
 
 for item in ${FILESET[@]:2}
 do
-	echo $item "your current usage is"
+	myecho $item "your current usage is"
 	USAGE_VAL=`mmlsquota -j $item mogpfs | grep mogpfs | awk -v N=3 '{print $N}'`
 	if [ $USAGE_VAL == 'no' ]; then usage="0" ; else usage=`expr $USAGE_VAL / 1024 / 1024`; fi
-	echo $usage"GB"
+	myecho $usage"GB"
 	
-	echo $item "your hard quota is"
+	myecho $item "your hard quota is"
 	HARD_VAL=`mmlsquota -j $item mogpfs | grep gpfs | awk -v N=5 '{print $N}'`
 	if [ -z $HARD_VAL ] ; then hard="0" ; else hard=`expr $HARD_VAL / 1024 / 1024`; fi
-	echo $hard"GB"
+	myecho $hard"GB"
 	
-	echo $item "your percentage usage is"
-	if [ -z $HARD_VAL ] ; then percentage="0" ; else PERCENTAGE_VAL= echo "scale=2; $usage*100/$hard" | bc | echo "%"; fi
+	myecho $item "your percentage usage is"
+	if [ -z $HARD_VAL ] ; then percentage="0" ; else percentage=`echo scale=2 "$usage*100/$hard" | bc`; fi
+	
+	ITEMROOT=$(mmlsfileset mogpfs | grep -w $item | awk '{print $3}')
+	echo "quota values= current usage: $usage GB, hard quota: $hard GB, percentage usage: $percentage %" > $ITEMROOT/quota.txt
+	
 done
 
