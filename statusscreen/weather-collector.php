cd787d23bd0e905ad39670692ce55023676ec938
jpc2000 <josh.cullum@cfms.org.uk>
2014-08-28 17:23:52 +0100

    Working on new status screen project

--- statusscreen/weather-collector.php
+++ statusscreen/weather-collector.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
 $datapath = "/opt/statusscreen/data/";
-$weather_feed = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Bristol,uk&mode=xml");
+$weather_feed = file_get_contents("http://weather.yahooapis.com/forecastrss?w=13963&u=c");
 $weather = simplexml_load_string($weather_feed);
 if(!$weather) die('weather failed');
 
 #print_r($weather->weather->current_conditions->temp_c);
-$temp_c = $weather->weather->current_conditions->temp_c['data'];
+$temp_c = $weather[''];
 $fp = fopen( $datapath . 'data-weather-temp', 'w' ); fwrite( $fp, $temp_c ); fclose( $fp );
 ?>
